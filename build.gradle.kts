d4d5b6c6fb5cb6f2f000ce20fa7e21d9f56c02f3
даниил черных <111225050+daniilcenyh@users.noreply.github.com>
2025-12-16 17:45:56 +0300

    hot fix swagger ui

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     java
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
-	id("org.springframework.boot") version "3.5.8"
-	id("io.spring.dependency-management") version "1.1.7"
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.5.8"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "edu.booking"
@@ -11,44 +11,50 @@ version = "0.0.1-SNAPSHOT"
 description = "Система бронирования номеров в отеле"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-
+    // Spring Boot starters
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
+
+    // Kotlin
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // Database
     implementation("org.postgresql:postgresql:42.7.8")
     implementation("org.liquibase:liquibase-core:4.33.0")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
 
+    // Lombok
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
 
-    implementation("org.springframework.boot:spring-boot-starter-validation")
+    // Swagger/OpenAPI
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
+    implementation("org.springdoc:springdoc-openapi-starter-common:2.8.6")
 
-//	testImplementation("org.springframework.boot:spring-boot-starter-web-test")
-//	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-//	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    // Тестирование
+    // testImplementation("org.springframework.boot:spring-boot-starter-test")
+    // testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    // testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-	}
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
 }
 
 tasks.withType<Test> {
+    useJUnitPlatform()
     jvmArgs("-Xshare:off")
-}
-
-tasks.withType<Test> {
-	useJUnitPlatform()
 }
